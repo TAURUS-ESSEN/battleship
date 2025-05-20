6b6af901f5c5207435198b4e3e3c2436c6eea008
TAURUS-ESSEN <reitarovea@gmail.com>
2025-05-20 13:14:04 +0200

    checkbeforeplacement() is fixed

--- src/gameboard.js
+++ src/gameboard.js
@@ -93,10 +93,10 @@ function checkBeforePlacement(x,y,shiplength, battlefield) {
     }
     if (direction === 1) {
         console.log('делаем все для вертикали')
-        let startX = Math.max(0, x - 1);
-        let endX = Math.min(10, x + shiplength + 1);
         if (x + shiplength <= 10) {
             console.log('идем вниз')
+            let startX = Math.max(0, x - 1);
+            let endX = Math.min(10, x + shiplength + 1);
             let test = battlefield.slice(startX, endX);
             let currentColumn = test.every(array => array[y] === '-') 
             if (y < 9) {
@@ -118,6 +118,34 @@ function checkBeforePlacement(x,y,shiplength, battlefield) {
                 startPointGenerator(battlefield, shiplength);
             }
         }
-        if (x-shiplength >= 0) { console.log('comming soon') }
+        else { 
+            console.log('идем вверх')
+            let startX = Math.max(10, x + 1);
+            let endX = Math.min(0, x - shiplength - 1);
+            // let test = battlefield.slice(startX, endX);
+            let test = battlefield.slice(endX, startX);
+            let currentColumn = test.every(array => array[y] === '-') 
+            if (y < 9) {
+                columnRight = test.every(array => array[y+1] === '-') 
+            }
+            else {  columnRight = true }
+            if (y > 0) {
+                columnLeft = test.every(array => array[y-1] === '-') 
+            }
+            else {  columnLeft = true }
+            console.log(currentColumn, columnLeft, columnRight)
+            if (currentColumn && columnLeft && columnRight) {
+                for (let i = 0; i < shiplength; i++){
+                    battlefield[x-i][y] = '3';
+                }
+                return (console.log('корабль вверх добавлен'))
+            }
+            else {
+                startPointGenerator(battlefield, shiplength);
+            }
+            // console.log('comming soon') 
+
+            
+        }
     }
 } 
