a44bfbf2e6ea6feae5c8305c5701d3ee40511dbf
TAURUS-ESSEN <reitarovea@gmail.com>
2025-05-20 14:24:41 +0200

    checkbeforeplacement() refactoring

--- src/gameboard.js
+++ src/gameboard.js
@@ -34,121 +34,75 @@ function startPointGenerator(battlefield, shiplength) {
 }
 
 function checkBeforePlacement(x,y,shiplength, battlefield) {
-    console.log('x =',x,'y =',y,'shiplength =',shiplength)
-    let lineDown = '';
-    let lineUp = '';
-    let columnRight = '-';
-    let columnLeft = '-';
-    const direction = Math.floor(Math.random()*2);
-    if (direction===0) {       
-        if (y+shiplength <= 10) {
-            console.log('идем вправо по горизонтали');
-            let startY = Math.max(0, y - 1);
-            let endY = Math.min(10, y + shiplength + 1);
-            console.log('startY', startY,'endY', endY);
-            let currentLine = battlefield[x].slice(startY, endY).every(cell => cell === '-');
-            if (x < 9) {
-                lineDown = battlefield[x + 1].slice(startY, endY).every(cell => cell === '-');
-            } else { lineDown = true }
-            if (x > 0) {
-                lineUp = battlefield[x - 1].slice(startY, endY).every(cell => cell === '-');
-            }
-            else { lineUp = true }
-            console.log('currentline',currentLine,'linedown',lineDown, 'lineUp', lineUp)
+    const direction = Math.floor(Math.random() * 2);
+    if (direction === 0) {       
+        if (y + shiplength <= 10) {
+            const startY = Math.max(0, y - 1);
+            const endY = Math.min(10, y + shiplength + 1);
+            const currentLine = battlefield[x].slice(startY, endY).every(cell => cell === '-');
+            const lineDown = x < 9 ? battlefield[x + 1].slice(startY, endY).every(cell => cell === '-') : true;
+            const lineUp = x > 0 ? battlefield[x - 1].slice(startY, endY).every(cell => cell === '-') : true;
             if (currentLine && lineUp && lineDown) {
-                for (let i = 0; i< shiplength; i++){
-                    battlefield[x][y+i] = '0';
+                for (let i = 0; i < shiplength; i++){
+                    battlefield[x][y + i] = '0';
                 }
-                return console.log('корабль успешно добавлен')
+                return console.log('корабль вправо успешно добавлен')
             }
             else {
-                console.log('не получилось. перезапуск')
                 return startPointGenerator(battlefield, shiplength);
             }
         }
         if (y-shiplength >= 0)  {
-        let startY = Math.max(0, y - shiplength - 1);
-        let endY = y + 1;
-            console.log('идем влево startY', startY,'endY', endY);
-            let currentLine = battlefield[x].slice(startY, endY).every(cell => cell === '-');
-            if (x < 9) {
-                lineDown = battlefield[x+1].slice(startY, endY).every(cell => cell === '-');
-            }
-            else { lineDown = true }
-            if (x > 0) {
-                lineUp = battlefield[x-1].slice(startY, endY).every(cell => cell === '-');
-            }
-            else { lineUp = true }
+            const startY = Math.max(0, y - shiplength - 1);
+            const endY = y + 1;
+            const currentLine = battlefield[x].slice(startY, endY).every(cell => cell === '-');
+            const lineDown = x < 9 ? battlefield[x+1].slice(startY, endY).every(cell => cell === '-') : true;
+            const lineUp = x > 0 ? battlefield[x-1].slice(startY, endY).every(cell => cell === '-') : true;
             if (currentLine && lineUp && lineDown) {
-                for (let i = 0; i< shiplength; i++) {
-                    battlefield[x][y-i] = '1';
+                for (let i = 0; i < shiplength; i++) {
+                    battlefield[x][y - i] = '1';
                 }
-                return console.log('корабль успешно добавлен')
+                return console.log('корабль влево успешно добавлен')
             }
             else {
-                console.log('не получилось. перезапуск')
                 return startPointGenerator(battlefield, shiplength);
             }
         } 
     }
     if (direction === 1) {
-        console.log('делаем все для вертикали')
         if (x + shiplength <= 10) {
-            console.log('идем вниз')
-            let startX = Math.max(0, x - 1);
-            let endX = Math.min(10, x + shiplength + 1);
-            let test = battlefield.slice(startX, endX);
-            let currentColumn = test.every(array => array[y] === '-') 
-            if (y < 9) {
-                columnRight = test.every(array => array[y+1] === '-') 
-            }
-            else {  columnRight = true }
-            if (y > 0) {
-                columnLeft = test.every(array => array[y-1] === '-') 
-            }
-            else {  columnLeft = true }
-            console.log(currentColumn, columnLeft, columnRight)
+            const startX = Math.max(0, x - 1);
+            const endX = Math.min(10, x + shiplength + 1);
+            const test = battlefield.slice(startX, endX);
+            const currentColumn = test.every(array => array[y] === '-') 
+            const columnRight = y < 9 ? test.every(array => array[y + 1] === '-') : true 
+            const columnLeft = y > 0 ? test.every(array => array[y - 1] === '-') : true 
             if (currentColumn && columnLeft && columnRight) {
                 for (let i = 0; i < shiplength; i++){
-                    battlefield[x+i][y] = '2';
+                    battlefield[x + i][y] = '2';
                 }
                 return (console.log('корабль вниз добавлен'))
             }
             else {
-                startPointGenerator(battlefield, shiplength);
+                return startPointGenerator(battlefield, shiplength);
             }
         }
-        else { 
-            console.log('идем вверх')
-            // let startX = Math.max(10, x + 1);
-            // let endX = Math.min(0, x - shiplength - 1);
-            // let test = battlefield.slice(startX, endX);
-            // let test = battlefield.slice(endX, startX);
-                let startX = Math.max(0, x - shiplength - 1);  // немного выше
-    let endX = x + 1;                              // до самой клетки x включительно
-    let test = battlefield.slice(startX, endX);    // срез по вертикали
-            let currentColumn = test.every(array => array[y] === '-') 
-            if (y < 9) {
-                columnRight = test.every(array => array[y+1] === '-') 
-            }
-            else {  columnRight = true }
-            if (y > 0) {
-                columnLeft = test.every(array => array[y-1] === '-') 
-            }
-            else {  columnLeft = true }
-            console.log(currentColumn, columnLeft, columnRight)
+        if (x - shiplength >= 0) { 
+            const startX = Math.max(0, x - shiplength - 1);
+            const endX = x + 1;                            
+            const test = battlefield.slice(startX, endX);    
+            const currentColumn = test.every(array => array[y] === '-') 
+            const columnRight = y < 9 ? test.every(array => array[y + 1] === '-') : true; 
+            const columnLeft = y > 0 ? test.every(array => array[y - 1] === '-') : true; 
             if (currentColumn && columnLeft && columnRight) {
                 for (let i = 0; i < shiplength; i++){
-                    battlefield[x-i][y] = '3';
+                    battlefield[x - i][y] = '3';
                 }
                 return (console.log('корабль вверх добавлен'))
             }
             else {
-                startPointGenerator(battlefield, shiplength);
+                return startPointGenerator(battlefield, shiplength);
             }
-            // console.log('comming soon') 
-
-            
         }
     }
 } 
--- src/index.js
+++ src/index.js
@@ -8,15 +8,12 @@ export const crusader2 = new Ship("crusader2",3);
 export const submarine = new Ship("submarine",2);
 export const minesweeper1 = new Ship("minesweeper1",1);
 export const minesweeper2 = new Ship("minesweeper2",1);
-// export const fleet = [linkor];
-// export const fleet = [linkor, crusader1, crusader2];
 export const fleet = [linkor, crusader1, crusader2, submarine, minesweeper1, minesweeper2];
 const player1Board = new Gameboard("player1GameBoard");
 const player2Board = new Gameboard("player2GameBoard");
 player1Board.placeShips(fleet);
-        // console.log(direction)
+player2Board.placeShips(fleet);
 
-// console.log(player1Board.battlefield[1])
 console.log ('          0    1    2    3    4    5    6    7    8    9 ')
 console.log('x0', player1Board.battlefield[0])
 console.log('x1',player1Board.battlefield[1])
